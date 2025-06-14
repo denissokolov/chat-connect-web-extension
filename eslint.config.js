9031f76a83a25dbc92a56eccc18551147c87dd98
denissokolov <dnssokolov@gmail.com>
2025-06-14 18:56:48 +0200

    setup eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,59 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import importPlugin from 'eslint-plugin-import'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import sonarjs from 'eslint-plugin-sonarjs'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      eslintPluginPrettierRecommended,
+      sonarjs.configs.recommended,
+    ],
+    files: ['**/*.{js,mjs,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'import/order': [
         'warn',
-        { allowConstantExport: true },
+        {
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
       ],
+      'prettier/prettier': 'warn',
+      'no-console': 'error',
+      'react/jsx-no-literals': 'error',
+      'require-await': 'error',
+      'no-param-reassign': 'error',
+      'no-return-await': 'error',
+      'no-redeclare': 'error',
+      'no-shadow': 'error',
+      'import/no-unresolved': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 )
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <div className="p-4 bg-red-100">Hello from the popup!</div>;
+  return <div className="p-4 bg-red-100">{'Hello from the popup!'}</div>
 }
