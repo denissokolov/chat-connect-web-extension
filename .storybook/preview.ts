77eab39101d43045411e16e29a31f8dbc3009348
denissokolov <dnssokolov@gmail.com>
2025-07-05 19:27:45 +0200

    add theme switcher to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import type { Preview } from '@storybook/react-vite'
 
 import '@/global.css'
@@ -5,6 +6,42 @@ import '@/global.css'
 window['IS_STORYBOOK'] = true
 
 const preview: Preview = {
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      toolbar: {
+        title: 'Theme',
+        items: [
+          {
+            value: 'light',
+            icon: 'sun',
+            title: 'Light',
+          },
+          {
+            value: 'dark',
+            icon: 'moon',
+            title: 'Dark',
+          },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
+  initialGlobals: {
+    theme: 'light',
+  },
+  decorators: [
+    (story, context) => {
+      const theme = context.globals.theme || 'light'
+      useEffect(() => {
+        document.body.classList.add(theme)
+        return () => {
+          document.body.classList.remove(theme)
+        }
+      }, [theme])
+      return story()
+    },
+  ],
   parameters: {
     controls: {
       matchers: {
